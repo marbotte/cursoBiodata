8fb004a55c9b6a4c2de9059164a3618a224f3fd8
marbotte <bottinmarius@gmail.com>
2023-11-12 09:12:44 -0500

    Search and match

---
+++ download/R_code/documentation/doc_R_buscarRelacionar.R
@@ -0,0 +1,28 @@
+## -------------------------------------------------------------------
+which(c(T,F,F,T))
+
+
+## -------------------------------------------------------------------
+vecLeyNormal <- rnorm(1000, mean = 0, sd = 1)
+(sup3 <- which(vecLeyNormal > 3))
+vecLeyNormal[sup3]
+
+
+## -------------------------------------------------------------------
+load("../Datos/bog_chinga.RData")
+mat_bog_chinga
+
+
+## -------------------------------------------------------------------
+(sup0 <- which(mat_bog_chinga > 0, arr.ind = T))
+mat_bog_chinga[sup0]
+
+
+## -------------------------------------------------------------------
+var <- c("A","A","b","B","Z","E","I")
+LETTERS
+
+m_var_LET<-match(var,LETTERS)
+
+```{.r .distill-force-highlighting-css}
+```
